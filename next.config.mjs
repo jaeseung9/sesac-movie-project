ca0b0da50046ce4b612b1da5d16ffb19c1e95ca5
jaeseung9 <seojaeseung9@gmail.com>
2025-11-06 16:15:42 +0900

    영상 업로드

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['image.tmdb.org'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
   },
+}
 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
+export default nextConfig
